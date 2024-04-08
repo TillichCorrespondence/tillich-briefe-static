ea77df13beac1f0e04aa0f2ed6a8f28fc9f2586e
csae8092 <peter.andorfer@oeaw.ac.at>
2024-04-08 13:16:01 +0200

    * used generic denormalizing scripts * simple script to rm lb from titles * minor change in view-type.xsl to match new back structure

--- fetch_data.sh
+++ fetch_data.sh
@@ -10,6 +10,15 @@ rm -rf data
 mv ./tillich-briefe-data-main/data .
 rm -rf ./main.zip ./tillich-briefe-data-main
 
+echo "fetching indices from tillich-entities"
+rm -rf data/indices
+curl -LO https://github.com/TillichCorrespondence/tillich-entities/archive/refs/heads/main.zip
+unzip main
+rm main.zip
+mkdir ./data/indices
+mv ./tillich-entities-main/data/indices ./data
+rm -rf tillich-entities-main
+
 
 echo "fetching imprint"
 rm ${IMPRINT_XML}
@@ -22,3 +31,6 @@ echo '<div lang="en">' >> ${IMPRINT_XML}
 curl https://imprint.acdh.oeaw.ac.at/${REDMINE_ID}en >> ${IMPRINT_XML}
 echo "</div>" >> ${IMPRINT_XML}
 echo "</root>" >> ${IMPRINT_XML}
+
+python check_files.py
+python remove_lb_from_title.py
--- transform.sh
+++ transform.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# This script transform the xmls from the data folder to html files. Intended for production use.
-echo "add back"
-python3 pythonscripts/add_back.py
 
-echo "add mentions"
-python3 pythonscripts/add_mentions.py
+echo "adding xml:id and next/prev attributes into root element of tei-docs"
+add-attributes -g "./data/editions/*.xml" -b "https://tillich-briefe.acdh.oeaw.ac.at/tillich-briefe"
+python check_files.py
 
-echo set typesense config
-cp typesense-config-prod.js html/js/typesense-config.js
+
+echo "denormalizing indices" 
+denormalize-indices -f "./data/editions/*.xml" -i "./data/indices/*.xml"
+
+python remove_notegrp_from_back.py
+# echo "add back"
+# python3 pythonscripts/add_back.py
+
+# echo "add mentions"
+# python3 pythonscripts/add_mentions.py
+
+# echo set typesense config
+# cp typesense-config-prod.js html/js/typesense-config.js
 
 echo "add Corresp Context"
 python3 pythonscripts/add_corresp_context.py
 
-echo "build ft-index"
-python3 pythonscripts/make_typesense_index.py --prod
+# echo "build ft-index"
+# python3 pythonscripts/make_typesense_index.py --prod
 
-echo "create app"
-ant
+# echo "create app"
+# ant
