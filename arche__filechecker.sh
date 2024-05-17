131a403b71326e6bf5c73e14c159e1b92f13e87d
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-17 11:18:31 +0200

    added script to run filechecker

--- arche__filechecker.sh
+++ arche__filechecker.sh
@@ -6,6 +6,6 @@ rm -rf fc_out && mkdir fc_out
 docker run \
   --rm \
   -v ${PWD}/fc_out:/reports \
-  -v ${PWD}/data:/data \
+  -v ${PWD}/to_ingest:/data \
   -v ~/.cvdupdate/database/:/var/lib/clamav \
   acdhch/arche-filechecker
