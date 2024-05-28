d1cdc9d7f3cf87cb264971b2b990fdae71b66622
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-28 09:21:11 +0200

    arche ingestion workflow set up

--- arche__ingest_binaries.sh
+++ arche__ingest_binaries.sh
@@ -5,4 +5,4 @@ docker run --rm \
   -v ${PWD}/to_ingest:/data \
   --entrypoint arche-import-binary \
   acdhch/arche-ingest \
-  /data https://id.acdh.oeaw.ac.at/${TOPCOLID} ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --skip not_exist
+  /data ${TOPCOLID}/ ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --skip not_exist
