cde331245511015943d43191d8170204d01018ab
csae8092 <p.andorfer@gmail.com>
2026-09-22 07:58:27 +0200

    feat: introduces LT_1934 env-variable

--- pyscripts/check_files.py
+++ pyscripts/check_files.py
@@ -22,3 +22,20 @@ if faulty:
         print(x)
 else:
     print("no errors, god job!")
+
+
+LT_1934 = os.environ.get("LT_1934")
+
+if LT_1934:
+    print("deleting all files from after 1933")
+    files = glob.glob("./data/editions/*.xml")
+    for x in files:
+        doc = TeiReader(x)
+        try:
+            date = doc.any_xpath(".//tei:date[@type='sort']/@when")[0]
+        except IndexError:
+            date = 2000
+            print(x)
+        year = int(date[:4])
+        if year > 1933:
+            os.remove(x)
